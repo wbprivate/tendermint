ef357758a8fb399e06fe21a3e47e333499b7c4da
M. J. Fromberger <fromberger@interchain.io>
2021-12-17 16:00:44 -0800

    crypto/sr25519: generate JSON wrappers

--- crypto/sr25519/encoding.go
+++ crypto/sr25519/encoding.go
@@ -1,13 +1,9 @@
 package sr25519
 
-import tmjson "github.com/tendermint/tendermint/libs/json"
-
 const (
 	PrivKeyName = "tendermint/PrivKeySr25519"
 	PubKeyName  = "tendermint/PubKeySr25519"
 )
 
-func init() {
-	tmjson.RegisterType(PubKey{}, PubKeyName)
-	tmjson.RegisterType(PrivKey{}, PrivKeyName)
-}
+//go:generate -command gen go run github.com/tendermint/tendermint/scripts/tmjson
+//go:generate gen -output generated.go -pkg sr25519 PubKey=tendermint/PubKeySr25519
