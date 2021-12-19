8a6461934b3c33d3d96cedf196e96af96785033e
M. J. Fromberger <fromberger@interchain.io>
2021-12-19 08:45:28 -0800

    crypto: register key types

--- crypto/sr25519/encoding.go
+++ crypto/sr25519/encoding.go
@@ -1,9 +1,16 @@
 package sr25519
 
+import "github.com/tendermint/tendermint/crypto"
+
 const (
-	PrivKeyName = "tendermint/PrivKeySr25519"
-	PubKeyName  = "tendermint/PubKeySr25519"
+	privKeyName = "tendermint/PrivKeySr25519"
+	pubKeyName  = "tendermint/PubKeySr25519"
 )
+
+func init() {
+	crypto.RegisterPubKeyType(pubKeyName, PubKey(nil))
+	crypto.RegisterPrivKeyType(privKeyName, PrivKey{})
+}
 
 //go:generate -command gen go run github.com/tendermint/tendermint/scripts/tmjson
 //go:generate gen -output generated.go -pkg sr25519 PubKey=tendermint/PubKeySr25519
